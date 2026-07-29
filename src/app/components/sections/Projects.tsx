1c92fb0b19cc317bd6924405e704dc7bac4f257a
N-Crossman <noahc1@hotmail.com>
2026-07-29 12:11:52 -0600

    Update project card styling to accept 4th project card

--- src/app/components/sections/Projects.tsx
+++ src/app/components/sections/Projects.tsx
@@ -25,7 +25,7 @@ export default function Projects() {
          I’m still working on projects. Check back soon!
         </motion.p>
       ) : (
-        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} index={index} />
           ))}
